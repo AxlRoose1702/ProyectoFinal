c65c1899447d99dac6034bb7af23a877632ac4fe
Pablito502 <pablolechuga502@gmail.com>
2025-05-10 10:41:27 -0600

    Agregue mi conexión a base de datos

--- Datos/CDConection.cs
+++ Datos/CDConection.cs
@@ -5,7 +5,8 @@ namespace ProyectoFinal
     public class CDConection
     {
         //Agregar su propia conexion a la base de datos
-        private SqlConnection conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=ProyectoFinal;Integrated Security=True;Trust Server Certificate=True");
+       // private SqlConnection conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=ProyectoFinal;Integrated Security=True;Trust Server Certificate=True");
+        private SqlConnection conn2 = new SqlConnection("Data Source=CASITA\\SQLEXPRESS;Initial Catalog=ProyectoFinal;Integrated Security=True;Trust Server Certificate=True");
 
 
         //Crear metodos para la apertura y cierre de la conexion a la base de datos.
