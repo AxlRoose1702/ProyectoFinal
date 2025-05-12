324b425100d89977e594156f01dcb5c3ae2657da
Pablito502 <pablolechuga502@gmail.com>
2025-05-12 14:03:00 -0600

    CDConection actualizado según las especificaciones del proyecto test, favor agregar cada uno su sql conection

--- Datos/CDConection.cs
+++ Datos/CDConection.cs
@@ -1,13 +1,37 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace ProyectoFinal
 {
     public class CDConection
     {
         //Agregar su propia conexion a la base de datos
-        private SqlConnection conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=ProyectoFinal;Integrated Security=True;Trust Server Certificate=True");
+        private SqlConnection conn = new SqlConnection("Data Source=CASITA\\SQLEXPRESS;Initial Catalog=db_hotel;Integrated Security=True;Encrypt=False");
+        //private SqlConnection conn = new SqlConnection("Data Source=MODIFICAR AQUI PAUL;Initial Catalog=db_hotel;Integrated Security=True;Encrypt=False");
+        //private SqlConnection conn = new SqlConnection("Data Source=MODIFICAR AQUI MOSQUITO;Initial Catalog=db_hotel;Integrated Security=True;Encrypt=False");
 
 
+        public SqlConnection MtdAbrirConexion()
+        {
+            //Verificar si la conexion esta cerrada y abrirla
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        public SqlConnection MtdCerrarConexion()
+        {
+            //Verificar si la conexion esta abierta y cerrarla
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            return conn;
+        }
+    
         //Crear metodos para la apertura y cierre de la conexion a la base de datos.
+
     }
 }
