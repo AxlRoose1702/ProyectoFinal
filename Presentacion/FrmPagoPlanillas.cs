c6b1bd8120e3e352ec28af95127f457b4d26e7c6
Pablito502 <pablolechuga502@gmail.com>
2025-05-22 18:08:17 -0600

    Botones, agregar, editar, eliminar y cancelar

--- Presentacion/FrmPagoPlanillas.cs
+++ Presentacion/FrmPagoPlanillas.cs
@@ -24,7 +24,7 @@ namespace ProyectoFinal
         public void mtdLimpiarCampos()
         {//metodo para limpiar los campos
             txtCodigoEmpleado.Clear();
-            txtCodigoPP.Text = "0";
+            txtCodigoPP.Clear();
             txtHorasExtras.Text = "0";
             cboxEstado.Text = "Seleccionar";
             lblBono.Text = "0";
@@ -100,10 +100,10 @@ namespace ProyectoFinal
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtCodigoEmpleado.Text) || string.IsNullOrEmpty(txtCodigoPP.Text)
-             || string.IsNullOrEmpty(txtHorasExtras.Text))
+            //siempre verificar que el load ya tenga la información necesaria y este creado el mtdConsultar
+            if (string.IsNullOrEmpty(txtCodigoEmpleado.Text) || string.IsNullOrEmpty(txtHorasExtras.Text))
             {
-                MessageBox.Show("Favor completar CodigoEmpleado, CodigoPP y HorasExtra", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Favor completar CodigoEmpleado y HorasExtra", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else if (string.IsNullOrEmpty(cboxEstado.Text) || cboxEstado.Text == "Seleccionar")
             {
@@ -159,8 +159,112 @@ namespace ProyectoFinal
         }
 
         private void txtHorasExtras_TextChanged(object sender, EventArgs e)
-        {
+        { //actualiza los datos de planilla instantaneamente
             MtdDatosPlanilla();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            //las condiciones if-else se repiten en el boton agregar y editar, pero no se pueden poner en un metodo
+            if (string.IsNullOrEmpty(txtCodigoEmpleado.Text) || string.IsNullOrEmpty(txtHorasExtras.Text))
+            {
+                MessageBox.Show("Favor completar CodigoEmpleado y HorasExtra", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrEmpty(cboxEstado.Text) || cboxEstado.Text == "Seleccionar")
+            {
+                MessageBox.Show("Favor seleccionar una opción de Estado", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (cd_pagoPlanillas.MtdVerificarEmpleado(int.Parse(txtCodigoEmpleado.Text)) != 1702)
+                {
+                    MessageBox.Show("El empleado no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                //Declaracion de todas las variables con todos los objetos del form
+                int codigoPP = int.Parse(txtCodigoPP.Text);
+                int CodigoEmpleado = int.Parse(txtCodigoEmpleado.Text);
+                double Salario = double.Parse(lblSalario.Text);
+                double Bono = double.Parse(lblBono.Text);
+                double MontoTotal = double.Parse(lblMontoTotal.Text);
+                int HorasExtra = int.Parse(txtHorasExtras.Text);
+                string Estado = cboxEstado.Text;
+                string UsuarioSistema = lblUsuarioSistema.Text;
+                DateTime fechaP = dateFechaPago.Value;
+                DateTime fechaS = dateFechaSistema.Value;
+
+                //ESTO ES LO MISMO QUE EN EL BOTON AGREGAR
+
+                try
+                {
+                    //se llama al metodo de ACTUALIZAR empleado en la clase CD
+                    cd_pagoPlanillas.MtdActualizarPP(codigoPP, CodigoEmpleado, fechaP, Salario, Bono, HorasExtra, MontoTotal, Estado, UsuarioSistema, fechaS);
+                    MessageBox.Show("Datos actualizados correctamente", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MtdConsultarPP(); //esto recarga la dgv para ver los cambios
+                    mtdLimpiarCampos(); //limpia los datos que hemos actualizado
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void dgvPagoPlanillas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {//ignorar
+        }
+
+        private void dgvPagoPlanillas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var FilaSeleccionada = dgvPagoPlanillas.SelectedRows[0];
+
+            if (FilaSeleccionada.Index == dgvPagoPlanillas.RowCount - 1)
+            {
+                MessageBox.Show("Seleccione una fila con datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                txtCodigoPP.Text = dgvPagoPlanillas.SelectedCells[0].Value.ToString();
+                txtCodigoEmpleado.Text = dgvPagoPlanillas.SelectedCells[1].Value.ToString();
+                dateFechaPago.Value = DateTime.Parse(dgvPagoPlanillas.SelectedCells[2].Value.ToString());
+                lblSalario.Text = dgvPagoPlanillas.SelectedCells[3].Value.ToString();
+                lblBono.Text = dgvPagoPlanillas.SelectedCells[4].Value.ToString();
+                txtHorasExtras.Text = dgvPagoPlanillas.SelectedCells[5].Value.ToString();
+                lblMontoTotal.Text = dgvPagoPlanillas.SelectedCells[6].Value.ToString();
+                cboxEstado.Text = dgvPagoPlanillas.SelectedCells[7].Value.ToString();
+                lblUsuarioSistema.Text = dgvPagoPlanillas.SelectedCells[8].Value.ToString();
+                dateFechaSistema.Value = DateTime.Parse(dgvPagoPlanillas.SelectedCells[9].Value.ToString());
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            mtdLimpiarCampos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCodigoPP.Text))
+            {
+                MessageBox.Show("Favor seleccionar fila a eliminar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                int CodigoPP = int.Parse(txtCodigoPP.Text);
+
+                try
+                {
+                    cd_pagoPlanillas.MtdEliminarPP(CodigoPP);
+                    MessageBox.Show("Usuario eliminado correctamente", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MtdConsultarPP();
+                    mtdLimpiarCampos();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
